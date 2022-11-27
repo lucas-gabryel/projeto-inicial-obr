29b2b8659c6d0642707caf661809aa2088ae982a
Lucas Santos <69809536+lucas-gabryel@users.noreply.github.com>
2022-11-26 23:07:47 -0300

    terminar o código de terça

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 basic.forever(function () {
+    // Com esses blocos e com o uso dos motores de 3 pinos foi possível fazer 3 dos 4 motores andarem e pararem manualmente.
     if (input.buttonIsPressed(Button.A)) {
-        pins.servoWritePin(AnalogPin.P0, 180)
+        pins.servoWritePin(AnalogPin.P0, 1)
+        pins.servoWritePin(AnalogPin.P0, 1)
+        pins.servoWritePin(AnalogPin.P0, 1)
+        pins.servoWritePin(AnalogPin.P0, 1)
     }
 })
